5a9412726658394991defdb8731a1ae66674d57f
Etudiant Openclassroom <agoraproject@hotmail.com>
2020-02-18 08:52:04 +0100

    before validation

--- InstagridTests/GridTest.swift
+++ InstagridTests/GridTest.swift
@@ -5,29 +5,18 @@
 //  Created by Haulin Jean-Philippe on 17/02/2020.
 //  Copyright © 2020 Concreteflowers. All rights reserved.
 //
-
+@testable import Instagrid
 import XCTest
 
 class GridTest: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testGivenLayoutIs1_whenChangingToLayout2_ThenDisplay3True() {
+        // Given
+        var grid: Grid = .layout1
+        
+        //When
+        grid = .layout2
+        
+        //Then
+        XCTAssert(grid.display[3] == true)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
